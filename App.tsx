e6eec8bb34e46c9f37b1d2ff44aaee9ec2cf1677
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-25 16:14:24 -0300

    Set status bar style to light

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <GarageScreen />
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
