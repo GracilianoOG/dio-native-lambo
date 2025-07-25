bfb4d01f50f7b0557c6f476bae985211393b9d5e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-25 16:10:08 -0300

    Store min and max ID of car api

--- src/constants/car.ts
+++ src/constants/car.ts
@@ -3,3 +3,6 @@ export const CAR_ASSETS_BASE_URL =
 
 export const CAR_API_BASE_URL =
   "https://digitalinnovationone.github.io/fake-data-api-lamborghini/api/lamborghini.json";
+
+export const CAR_MIN_ID = 1;
+export const CAR_MAX_ID = 10;
