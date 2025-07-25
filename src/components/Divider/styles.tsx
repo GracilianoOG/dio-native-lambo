4788a54fbb3258cecdbe95248ac5b1d4a0b90e11
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-25 13:22:28 -0300

    Create Divider component

---
+++ src/components/Divider/styles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  divider: {
+    alignItems: "center",
+    backgroundColor: "#01a6b3",
+    height: 1,
+    marginVertical: 10,
+    width: "80%",
+  },
+});
