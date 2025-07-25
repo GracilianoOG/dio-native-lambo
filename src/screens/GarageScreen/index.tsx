cb02363fc22aa8480a0d5b4eee30b3d4d2491017
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-25 12:30:29 -0300

    Create styles for Garage screen

--- src/screens/GarageScreen/index.tsx
+++ src/screens/GarageScreen/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { styles } from "./styles";
 
 const GarageScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Works</Text>
     </View>
   );
---
+++ src/screens/GarageScreen/styles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#1e1e20",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
