c1cbe17c41d3e6476e6ed2fcb0fc6780686d9b5e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-25 12:28:43 -0300

    Fix screen import

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import GarageScreen from "./src/screens/GarageScreen/GarageScreen.component";
+import GarageScreen from "./src/screens/GarageScreen";
 
 export default function App() {
   return (
