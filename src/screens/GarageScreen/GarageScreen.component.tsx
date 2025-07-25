1c66fe38899afc01107332765081179317e99df0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-25 12:24:06 -0300

    Create GarageScreen

---
+++ src/screens/GarageScreen/GarageScreen.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const GarageScreen = () => {
+  return (
+    <View>
+      <Text>Works</Text>
+    </View>
+  );
+};
+
+export default GarageScreen;
