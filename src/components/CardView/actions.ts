ebd94f622e2e009453470efa5beda2548f69ad12
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-25 15:36:32 -0300

    Implement loadCarData to fetch a car

--- src/components/CardView/actions.ts
+++ src/components/CardView/actions.ts
@@ -1,4 +1,20 @@
-export const loadCarData = async () => {};
+import { getCarData } from "../../api/getCars";
+import { CarModel } from "./props";
+
+export const loadCarData = async (
+  id: number,
+  setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>
+) => {
+  try {
+    const response = await getCarData(id);
+    if (response) {
+      setCarData(response);
+    }
+  } catch (error) {
+    setCarData(null);
+    console.log("Erro ao buscar os dados na API: ", error);
+  }
+};
 
 export const handlePreviousItem = async () => {};
 
