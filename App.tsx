f19801d3776731be5e23c25d5ebe2c2c47710dc2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-28 16:20:59 -0300

    Add purchase screen to navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import GarageScreen from "./src/screens/GarageScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import PurchaseScreen from "./src/screens/PurchaseScreen";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ export default function App() {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Home" component={GarageScreen} />
+        <Screen name="Purchase" component={PurchaseScreen} />
       </Navigator>
     </NavigationContainer>
   );
