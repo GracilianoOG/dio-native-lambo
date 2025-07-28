5343076351ba7d173282f71c13f1bcf733f6af1e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-28 17:31:03 -0300

    Remove unused import

--- src/screens/PurchaseScreen/index.tsx
+++ src/screens/PurchaseScreen/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text } from "react-native";
 import { styles } from "./styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CAR_ASSETS_BASE_URL } from "../../constants/car";
