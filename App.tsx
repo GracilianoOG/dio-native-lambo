813b30490e1836f9252983ba545703130ee324ca
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-28 16:09:21 -0300

    Remove view and its styles

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
 import GarageScreen from "./src/screens/GarageScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
       <GarageScreen />
       <StatusBar style="light" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
