8fc8440abcc1c3447db3b5c894aa02d735193601
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-28 17:29:02 -0300

    Create type for CardView

--- src/components/CardView/props.ts
+++ src/components/CardView/props.ts
@@ -1,6 +1,10 @@
+import { GarageScreenProps } from "../../screens/GarageScreen/props";
+
 export interface CarModel {
   id: number;
   carName: string;
   releaseYear: number;
   price: string;
 }
+
+export type CardViewProps = {} & GarageScreenProps;
