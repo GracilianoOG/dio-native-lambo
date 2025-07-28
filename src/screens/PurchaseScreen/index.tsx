6baabd90295c99d5ea3a681e4b2315b2991340f8
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-28 16:20:25 -0300

    Create purchase screen

---
+++ src/screens/PurchaseScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+const PurchaseScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Works</Text>
+    </View>
+  );
+};
+
+export default PurchaseScreen;
