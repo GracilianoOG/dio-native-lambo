caeeb28db81875c167d7cd926838c69761131cf7
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-28 16:14:56 -0300

    Create navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import GarageScreen from "./src/screens/GarageScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function App() {
-  return <GarageScreen />;
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="Home" component={GarageScreen} />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
