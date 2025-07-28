38fa5e7928f009e6f6d4c833fc164a0089a4fd2d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-28 16:05:15 -0300

    Show confirmation dialog when buying a vehicle

--- src/components/BuyButton/index.tsx
+++ src/components/BuyButton/index.tsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { styles } from "./styles";
 import { BuyButtonProps } from "./props";
 
 const BuyButton = ({ title }: BuyButtonProps) => {
+  const handlePress = () => {
+    Alert.alert(
+      "Confirm purchase",
+      "Do you want to purchase that lamborghini?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Buy",
+          onPress: () => {
+            console.log("Car bought");
+          },
+        },
+      ]
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
         <AntDesign
           name="shoppingcart"
           size={24}
